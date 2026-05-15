2c29c63f6fd436e6748d8490b3574deb88932251
shalaga44 <shalaga44@e.email>
2026-05-16 03:45:07 +0400

    Enable review by default

--- src/main/kotlin/dev/shalaga44/storage/tables/GuildSettingsTable.kt
+++ src/main/kotlin/dev/shalaga44/storage/tables/GuildSettingsTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 object GuildSettingsTable : LongIdTable("guild_settings") {
     val confessionChannelId = long("confession_channel_id")
-    val reviewEnabled = bool("review_enabled").default(false)
+    val reviewEnabled = bool("review_enabled").default(true)
     val loggingChannelId = long("logging_channel_id").nullable()
     val moderatorRoleIds = text("moderator_role_ids").default("")
 }
